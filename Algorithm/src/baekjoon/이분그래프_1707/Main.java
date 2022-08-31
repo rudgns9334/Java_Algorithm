a0a6d92c339c2c81d8a4d214a11845f5a8a32532
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-08-31 17:11:09 +0900

    0831 스터디

---
+++ Algorithm/src/baekjoon/이분그래프_1707/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.이분그래프_1707;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int K;
+	static boolean[] color;
+	static boolean[] visit;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		K = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= K; t++) {
+			
+		}
+	}
+	
+	static class Vertex{
+		
+	}
+
+}
