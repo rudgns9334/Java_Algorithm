66d901289d5658e9110c89258280e9a7c62850b8
rudgns9334 <rudgns8285@naver.com>
2022-09-08 18:27:38 +0900

    추석이라 쉰대요

---
+++ Algorithm/src/baekjoon/용액_2467/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.용액_2467;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int N;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+
+	}
+
+}
