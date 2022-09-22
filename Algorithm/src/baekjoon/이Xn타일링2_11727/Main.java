4127737d7e2d83eb1ff128590371801286ea2eee
rudgns9334 <rudgns8285@naver.com>
2022-09-22 23:27:56 +0900

    타일링

--- Algorithm/src/baekjoon/이Xn타일링2_11727/Main.java
+++ Algorithm/src/baekjoon/이Xn타일링2_11727/Main.java
@@ -5,18 +5,17 @@ import java.io.InputStreamReader;
 
 public class Main {
 	
-	static int N;
-	static long[] dp;
-	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		dp = new long[N+1];
-		dp[0] = 0;
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
+		dp[0] = 1;
 		dp[1] = 1;
-		dp[2] = 3;
-		for (int i = 3; i <= N; i++) {
-			
+		for (int i = 2; i <= N; i++) {
+			dp[i] = (dp[i-1] + dp[i-2] * 2)%10007;
 		}
+		
+		
+		System.out.println(dp[N]);
 	}
 }
