2f2cbe604084b3a6324eb69326e55444905dee50
rudgns9334 <rudgns9334@gmail.com>
2022-11-01 18:03:14 +0900

    상어중

---
+++ Algorithm/src/baekjoon/상어중학교_21609/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.상어중학교_21609;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
