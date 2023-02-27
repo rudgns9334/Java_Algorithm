f9f08a26ab553fa3c399c4f9939417f2973d0de1
rudgns <rudgns8285@naver.com>
2023-02-27 21:50:43 +0900

    왜안됨

--- Algorithm/src/baekjoon/미네랄_2933/Main.java
+++ Algorithm/src/baekjoon/미네랄_2933/Main.java
@@ -2,20 +2,24 @@ package baekjoon.미네랄_2933;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int R,C,N, total;
+	static int R,C,N,Y;
 	static char[][] map;
 	static int[] high;
 	
-	static boolean[][] visit;
+	static PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n2.y - n1.y);
 	
+	static boolean[][] visit;
+					// 하 상 우 좌
 	static int[] dy = {1, -1, 0, 0};
-	static int[] dx = {0, 0, -1, 1};
+	static int[] dx = {0, 0, 1, -1};
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,18 +27,10 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		total = 0;
-		
 		map = new char[R][C];
 		
 		for (int i = 0; i < R; i++) {
 			map[i] = br.readLine().toCharArray();
-			for (int j = 0; j < C; j++) {
-				if(map[i][j] == 'X') {
-					total++;
-				}
-				
-			}
 		}
 		
 		N = Integer.parseInt(br.readLine());
@@ -45,118 +41,144 @@ public class Main {
 			start(i);
 		}
 		
-		
-		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
 		
 	}
 	
 	static void start(int cnt) {
 		int y = R-high[cnt];
-		
+		pq.clear();
 		if(cnt%2 == 0) {
 			// 왼쪽에서 오른쪽으로 d = 3
 			int x = -1;
 			for (int i = 0; i < C; i++) {
-				x = x + dx[3];
-				if(map[y][x] == 'X') {
+				x = x + dx[2];
+				if(map[y][x] == 'x') {
 					map[y][x] = '.';
-					total--;
-					if(check()) {
-						gravity(y,i);
+					if(check(y,x,1)) {
+						gravity();
 					}
+					break;
 				}
 			}
 		}else {
 			// 오른쪽에서 왼쪽으로 d = 2
 			int x = C;
 			for (int i = 0; i < C; i++) {
-				x = x + dx[2];
-				if(map[y][x] == 'X') {
+				x = x + dx[3];
+				if(map[y][x] == 'x') {
 					map[y][x] = '.';
-					total--;
-					if(check()) {
-						gravity(y,i);
+					if(check(y,x,2)) {
+						gravity();
+					}
+					break;
+				}
+			}
+		}
+	}
+	
+	static boolean check(int y, int x, int type) {
+		visit = new boolean[R][C];
+//		System.out.println("y : " + y + " x : " + x);
+		for (int i = 1; i < 4; i=i+type) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+//			System.out.println(i);
+			if(ny<0 || nx<0 || ny>=R || nx>=C || visit[ny][nx]) continue;
+			
+//			System.out.println("inddd y : " + ny + " x : " + nx);
+			if(map[ny][nx] == 'x') {
+				
+				if(dfs(ny,nx)) {
+					return false;
+				}else {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	static boolean dfs(int y, int x) {
+		if(y == R-1) {
+//			System.out.println("dfs in y : " + y + " x : " + x);
+			return true;
+		}
+		pq.add(new Node(y,x));
+		visit[y][x] = true;
+		boolean ok = false;
+		for (int i = 0; i < 4; i++) {
+			if(ok) break;
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			
+			if(ny<0 || nx<0 || ny>=R || nx>=C || visit[ny][nx]) continue;
+			if(map[ny][nx] == 'x') {
+				ok = dfs(ny,nx);
+			}
+		}
+		
+		return ok;
+	}
+		
+	static void gravity() {
+		// 상 좌 우 만 체크
+		int size = pq.size();
+		
+		int minH = R+1;
+		Iterator<Node> its = pq.iterator();
+//		System.out.println(size);
+		// 최소 높이 차이 구하기
+		while(its.hasNext()) {
+			
+			Node n = its.next();
+			
+			Y = n.y+1;
+			if(map[Y][n.x] == 'x') continue;
+			
+			while(Y < R) {
+				if(map[Y][n.x] == 'x') {
+					if(pq.stream().noneMatch(nn -> (nn.x == n.x) && (nn.y == Y) )) {
+						if(minH > Y - n.y) {
+							
+							minH = Y - n.y;
+//							System.out.println("y : " + n.y + " x : " + n.x + " minH : " + minH );
+						}
+					}
+					
+					break;
+				}
+				if(Y == R-1) {
+					if(minH > Y-n.y) {
+						minH = Y - n.y + 1;
 					}
 				}
+				Y++;
 			}
-		}
-	}
-	
-	static boolean check() {
-		visit = new boolean[R][C];
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if(map[i][j] == 'X' && !visit[i][j]) {
-					int n = bfs(i,j);
-					// 탐색 개수가 전체 개수와 같으면
-					if(n == total) return false;
-					// 같지 않으면
-					else return true;
-				}
-			}
-		}
-		
-		
-		return true;
-	}
-	
-	static int bfs(int y, int x) {
-		
-		int cnt = 1;
-		
-		visit[y][x] = true;
-		Queue<Node> q = new ArrayDeque<>();
-		q.add(new Node(y,x,1));
-		
-		while(!q.isEmpty()) {
-			Node node = q.poll();
 			
-			if(node.cnt > cnt) cnt = node.cnt;
-			
-			for (int d = 0; d < 4; d++) {
-				int ny = node.y + dy[d];
-				int nx = node.x + dx[d];
-				
-				if(ny<0 || nx<0 || ny>=R || nx>=C) continue;
-				if(visit[ny][nx]) continue;
-				if(map[ny][nx] == 'X') {
-					visit[ny][nx] = true;
-					q.add(new Node(ny, nx, node.cnt + 1));
-				}
-			}
 		}
-		
-		return cnt;
-	}
-	
-	static void gravity(int y, int x) {
-		// 상 좌 우 만 체크
-		int nnY = -1;
-		int nnX = -1;
-		for (int d = 1; d < 4; d++) {
-			int ny = y + dy[d];
-			int nx = x + dx[d];
-			if(ny<0 || nx<0 || ny>=R || nx>=C) continue;
-			if(map[ny][nx] == 'X') {
-				nnY = ny;
-				nnX = nx;
-				break;
-			}
-		}
-		if(nnY == -1) {
-			return;
+//		System.out.println(minH);
+		// 옮기기
+		for (int i = 0; i < size; i++) {
+			Node n = pq.poll();
+			map[n.y][n.x] = '.';
+			map[n.y+minH-1][n.x] = 'x';
 		}
 		
 		
 	}
 	
 	static class Node{
-		int y,x,cnt;
+		int y,x;
 		
-		public Node(int y, int x, int cnt){
+		public Node(int y, int x){
 			this.y = y;
 			this.x = x;
-			this.cnt = cnt;
 		}
 	}
 }
